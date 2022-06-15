4c99f7bf6aef5ab638e7c65a860b3df976b79ee5
yessawab <yassine.essawabi@gmail.com>
2022-06-15 12:06:27 +0200

    Github actions workflow

--- environments/databricks-department-clusters-pat/backend.tf
+++ environments/databricks-department-clusters-pat/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "home"
-    storage_account_name = "tfbackendsahome"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
+    #resource_group_name  = "home"
+    #storage_account_name = "tfbackendsahome"
+    #container_name       = "tfstate"
+    #key                  = "prod.terraform.tfstate"
   }
 }
